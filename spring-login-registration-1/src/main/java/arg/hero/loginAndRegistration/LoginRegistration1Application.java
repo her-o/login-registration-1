a4537e811c6ea496002dc52d74aa816e07083073
Hernan <hernan.olmos11@gmail.com>
2021-12-10 21:29:00 -0300

    first commit - started backend

---
+++ spring-login-registration-1/src/main/java/arg/hero/loginAndRegistration/LoginRegistration1Application.java
@@ -0,0 +1,13 @@
+package arg.hero.loginAndRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistration1Application.class, args);
+	}
+
+}
---
+++ spring-login-registration-1/src/test/java/arg/hero/loginAndRegistration/LoginRegistration1ApplicationTests.java
@@ -0,0 +1,13 @@
+package arg.hero.loginAndRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistration1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
